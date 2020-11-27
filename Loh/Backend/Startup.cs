2f39d1198f8b3b0322c4571387c8e07fcee7f3b2
Sergey <gooser@bk.ru>
2020-11-27 18:43:10 +0300

    feat: базовая поддержка WebSocket

--- Loh/Backend/Startup.cs
+++ Loh/Backend/Startup.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -43,17 +49,54 @@ namespace Loh.Backend
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //WebSockets
+
+            var webSocketOptions = new WebSocketOptions()
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(120),
+                ReceiveBufferSize = 4 * 1024
+            };
+
+            app.UseWebSockets(webSocketOptions);
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        using (WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync())
+                        {
+                            await Echo(context, webSocket);
+                        }
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+
+                else
+                {
+                    await next();
+                }
+            });
+            //app.UseFileServer();
+            /*
             app.UseStaticFiles();
+            app.UseHttpsRedirection();
             app.UseSpaStaticFiles();
 
+            */
+            ///////////////
+
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 //endpoints.MapControllerRoute(
-               //     name: "default",
-               //     pattern: "{controller}/{action=Index}/{id?}");
+                //     name: "default",
+                //     pattern: "{controller}/{action=Index}/{id?}");
                 endpoints.MapControllers();
             });
 
@@ -66,6 +109,23 @@ namespace Loh.Backend
                     spa.UseReactDevelopmentServer(npmScript: "start");
                 }
             });
+
+        }
+
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                var j = Encoding.UTF8.GetString(buffer).Replace("\0","");
+                var o = $"принял '{j}' и вернул обратно";
+                var n = Encoding.UTF8.GetBytes(o);
+                await webSocket.SendAsync(new ArraySegment<byte>(n, 0, n.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
     }
 }
